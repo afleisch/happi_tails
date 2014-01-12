46552b0df3848a6f639f93f218a30dbef6c0ca5f
afleisch <afleisch@gmail.com>
2014-01-12 14:03:24 -0800

    phase 3

--- happitails.rb
+++ happitails.rb
@@ -6,17 +6,35 @@ require './client'
 
 shelter= Shelter.new("happitails")
 
-frank = Animal.new("frank", 2, "male", "dog", 3)
+def add_animal()
+	puts "Please enter the animal's info"
+	print "name:"; name = gets.chomp
+	print "age:"; age = gets.chomp
+	print "gender:"; gender = gets.chomp
+	print "what kind of animal is it?"; species = gets.chomp
+	print "how many toys does it have?"; toys = gets.chomp
+	Animal.new(name, age, gender, species, toys)
+  end
 
-shelter.animals << frank
+# shelter.animals << add_animal()
+
+# p shelter.animals
+
+def add_client()
+	puts "Please enter client info"
+	print "name:"; name = gets.chomp
+	print "no. of children:"; num_children = gets.chomp
+	print "age:"; age = gets.chomp
+	Client.new(name, num_children, age)
+end
+
+shelter.animals << add_animal()
 
 p shelter.animals
 
-mary = Client.new("mary", 2, 34)
 
-shelter.clients << mary
+shelter.clients << add_client()
 
 p shelter.clients
-
 
 
