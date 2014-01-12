ddd3e3411de5b260b7956851b07711d00556c45f
afleisch <afleisch@gmail.com>
2014-01-12 12:55:58 -0800

    phase 1

--- happitails.rb
+++ happitails.rb
@@ -4,7 +4,17 @@ require './client'
 
 
 
+shelter= Shelter.new("happitails")
 
+p shelter
+
+frank = Animal.new("frank", 2, "male", "dog", 3)
+
+p frank
+
+mary = Client.new("mary", 2, 34)
+
+p mary
 
 
 
